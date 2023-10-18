46a3a6d6233d4970b21b8ba293f388d78add8061
implementacaoteste <106354515+implementacaoteste@users.noreply.github.com>
2023-10-18 11:28:07 -0300

    Criação das assinaturas dos métodos da camada DAL
    O próximo passo vai ser implementar os métodos.

---
+++ GestaoComercial/DAL/UsuarioDAL.cs
@@ -0,0 +1,24 @@
+﻿using Models;
+
+namespace DAL
+{
+    public class UsuarioDAL
+    {
+        public void Inserir(Usuario _usuario)
+        {
+
+        }
+        public void Alterar(Usuario _usuario)
+        {
+
+        }
+        public void Excluir(int _id)
+        {
+
+        }
+        public List<Usuario> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
